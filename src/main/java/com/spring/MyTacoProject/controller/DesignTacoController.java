8b233241e6c2c8b749be90b2f7562b51abc4995f
ankurpm <ankurpm90@gmail.com>
2023-07-26 20:42:31 -0400

    Day 2

--- src/main/java/com/spring/MyTacoProject/controller/DesignTacoController.java
+++ src/main/java/com/spring/MyTacoProject/controller/DesignTacoController.java
@@ -3,6 +3,7 @@ package com.spring.MyTacoProject.controller;
 import com.spring.MyTacoProject.model.Ingredient;
 import com.spring.MyTacoProject.model.Ingredient.Type;
 import com.spring.MyTacoProject.model.Taco;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,17 +30,18 @@ public class DesignTacoController {
             model.addAttribute(type.toString().toLowerCase(),
                     filterByType(ingredients, type));
         }
+        //you will fill this attribute to create a new taco
         model.addAttribute("design", new Taco());
-        return "design1";
+        return "design";
     }
 
     @PostMapping
-    public String processDesign(Taco design, Errors errors) {
+    public String processDesign(@Valid Taco design, Errors errors) {
+
         if(errors.hasErrors()){
             return "design";
         }
-// Save the taco design...
-// We'll do this in chapter 3
+
         log.info("Processing design: " + design);
         return "redirect:/orders/current";
     }
--- src/main/java/com/spring/MyTacoProject/model/Taco.java
+++ src/main/java/com/spring/MyTacoProject/model/Taco.java
@@ -1,13 +1,21 @@
 package com.spring.MyTacoProject.model;
 
 
+
 import lombok.Data;
 
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
 public class Taco {
 
+    @NotNull
+    @Size(min = 5, message = "Name should be at least 5 characters long")
     private String name;
+
+    @NotNull
     private List<String> ingredients;
 }
